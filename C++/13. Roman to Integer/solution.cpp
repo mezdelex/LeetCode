ed9161fdd727eabe256a19f00ddee5a1704eb9a5
Mezdelex <alexcondegomez@gmail.com>
2022-04-18 17:16:18 +0200

    eliminados los breaks del switch

--- C++/13. Roman to Integer/solution.cpp
+++ C++/13. Roman to Integer/solution.cpp
@@ -13,25 +13,18 @@ public:
         {
         case 'I':
             return 1;
-            break;
         case 'V':
             return 5;
-            break;
         case 'X':
             return 10;
-            break;
         case 'L':
             return 50;
-            break;
         case 'C':
             return 100;
-            break;
         case 'D':
             return 500;
-            break;
         case 'M':
             return 1000;
-            break;
         default:
             return 0;
         }
