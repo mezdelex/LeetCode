3a64d89913cc8fa3842bb3cb8d2f7dbfc0fbf986
Mezdelex <alexcondegomez@gmail.com>
2022-05-13 17:14:02 +0200

    added C# 14. Longest Common Prefix solution using LinQ

---
+++ C#/14. Longest Common Prefix/Solution.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        string prefix = "";
+
+        int minLength = strs.Select(str => str.Length).Min();
+
+        for (int i = 0; i < minLength; ++i)
+        {
+            IEnumerable<char> characters = strs.Select(str => str[i]).Distinct();
+
+            if (characters.Count() > 1)
+                break;
+
+            prefix += characters.LastOrDefault();
+        }
+
+        return prefix;
+    }
+}
