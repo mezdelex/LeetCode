a280971ffdda91111e44ebcecb15c6131d3a0f89
Mezdelex <alexcondegomez@gmail.com>
2022-08-20 22:13:29 +0200

    Modified solution 13. Roman to Integer

--- rust/src/_13_roman_to_integer.rs
+++ rust/src/_13_roman_to_integer.rs
@@ -4,8 +4,7 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn roman_to_int(s: String) -> i32 {
-        // into_iter + collect is a way to populate the HashMap in the initialization
-        let map: HashMap<char, i32> = vec![
+        let map: HashMap<char, i32> = HashMap::from([
             ('I', 1),
             ('V', 5),
             ('X', 10),
@@ -13,22 +12,23 @@ impl Solution {
             ('C', 100),
             ('D', 500),
             ('M', 1000),
-        ]
-        .into_iter()
-        .collect();
+        ]);
 
         let mut sum = 0;
         let mut last = 0;
 
         for c in s.chars() {
-            if let Some(&value) = map.get(&c) {
-                if value > last {
-                    sum += value - last - last; // Compensate previous iteration
-                } else {
-                    sum += value
-                }
+            match map.get(&c) {
+                Some(&value) => {
+                    if value > last {
+                        sum += value - last - last;
+                    } else {
+                        sum += value;
+                    }
 
-                last = value;
+                    last = value;
+                }
+                _ => {} // None -> Default if no matches found
             }
         }
 
